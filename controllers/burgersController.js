eb4307c5c9bb9671e17295d240504fc68384694b
Krista <krista.coding@gmail.com>
2020-11-25 16:15:03 -0500

    added / line

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,6 +5,10 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "burgers"));
+});
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/burgers", function(req, res) {
   burger.selectAll(function(data) {

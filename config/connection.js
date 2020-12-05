958ba511cef50b0a7abb496c16df5f94851d3c32
Krista <krista.coding@gmail.com>
2020-12-05 14:25:13 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,25 @@
 var mysql = require("mysql");
 var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ILv3Oc8!",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "ILv3Oc8!",
+//   database: "burgers_db"
+//  });
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(prcess.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "ILv3Oc8!",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -18,18 +30,7 @@ connection.connect(function(err) {
   console.log("connected as id ");
 });
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(prcess.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    username: "eiikccv3im6so4ed",
-    password: "zs2sm335hnr103zg",
-    host: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    database:"hj6or5hx3kpm0ubx" 
-  });
-};
-
-connection.connect();
+// connection.connect();
 
 
 // Export connection for our ORM to use.
--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,12 +5,8 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "burgers"));
-});
-
 // Create all our routes and set up logic within those routes where required.
-router.get("/burgers", function(req, res) {
+router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
